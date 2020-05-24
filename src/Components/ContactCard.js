eacbacbd037468d97985627e9f8a5deae4529bfe
lex-marie790 <lexi.marie790@gmail.com>
2020-05-24 02:38:43 -0700

    add toggle button and useState for age

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactCard = () => {
+    const [showAge, setShowAge] = useState(true);
+    const handleClick = () => {
+        setShowAge(!showAge);
+    }
+
     return (
       <div className="contact-card">
         <img src="https://via.placeholder.com/150" alt="profile" />
         <div className="user-details">
           <p>Name: Jenny Han</p>
           <p>Email: Jenny.han@notreal.com</p>
-          <p>Age: 25</p>
+          {/*if the showAge varua==ial is true, render the age, if not, render nothing  */}
+          {/* {showAge === true ?<p>Age: 25</p> : null} */}
+          <button onClick={handleClick}>Toggle Age</button>
+          {showAge && <p>Age: 25</p> }
         </div>
       </div>
      ); //this is whats returned by the component & contains markup, converted to html

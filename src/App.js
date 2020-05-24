617719c012fcbe767322ea5f108bd807d1f19ed0
lex-marie790 <lexi.marie790@gmail.com>
2020-05-24 02:00:56 -0700

    First commit - outline set up with notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// step one import react
+import React from "react";
 
-function App() {
+//step 2 declare a function, export default App
+//step 3 add div and h1 tags - JSX Javascript XML
+
+const App = () => {
+
+  const message = "This is my first variable rendered in JSX";
+
+  const handleClick = () => {
+    alert("you clicked the message!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>
+        This is our React World
+      </h1>
+      <h2 onClick = {handleClick}>
+        {message} 
+      </h2>
     </div>
-  );
+  ); //this is whats returned by the component & contains markup, converted to html
+
 }
 
 export default App;
+
+// React creates an element tree:
+// React.createElement(component, props, ...children)
+// component: html element being created - h1 and div
+// props:
+// children: array of HTML elements nested within this element
+// {} means execute as JS
+// index.html is the skeleton, loads when the app starts and imports App component

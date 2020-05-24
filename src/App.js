5e0794c88a8693f037feb9de1c18cfde73f4920c
lex-marie790 <lexi.marie790@gmail.com>
2020-05-24 02:59:08 -0700

    created contacts file and passed data in the array to component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 import "./styles.css";
 
 import ContactCard from "./Components/ContactCard";
+import contact from "./Components/contacts"
+import contacts from "./Components/contacts";
 
 //step 2 declare a function, export default App
 //step 3 add div and h1 tags - JSX Javascript XML
@@ -12,24 +14,14 @@ const App = () => {
 
   return (
     <>
-    <ContactCard
-      avatar = "https://via.placeholder.com/150"
-      name="Jenny Han"
-      email="jenny.han@notreal.com"
-      age={25}
-    />
-    <ContactCard 
-      avatar = "https://via.placeholder.com/150"
-      name="Jason Long"
-      email="jason.long@notreal.com"
-      age={45}
-    />
-    <ContactCard
-      avatar = "https://via.placeholder.com/150"
-      name="Peter Pan"
-      email="peter.pan@notreal.com"
-      age={100}
-    />
+    {contacts.map(contact=> (
+      <ContactCard
+        avatar = "https://via.placeholder.com/150"
+        name={contact.name}
+        email={contact.email}
+        age={contact.age}
+      />
+    ))}
     </>
   );
 }
@@ -46,3 +38,10 @@ export default App;
 // index.html is the skeleton, loads when the app starts and imports App component
 // state: basically an object that represents a part of an app that can change, which the UI “reacts” to. State can be anything; objects, booleans, arrays, strings or integers
 // Props: defined using quoted text (e.g. name=“Jenny Han”) or within braces (e.g. age={25}. If we leave out the braces for anything other than strings things start to break — age=25 );
+
+
+// To display a list of components, we: (imported from contacts.js)
+
+// 1. Loop over the array using .map()
+// 2. For each item in the array, create a new ContactCard component
+// 3. Pass the data from each object in the array to the ContactCard component as props
---
+++ src/Components/contacts.js
@@ -0,0 +1,7 @@
+const contacts = [
+    {name: "Jenny Han", email: "jenny.han@notreal.com", age: 25},
+    {name: "Jason Long", email: "jason.longn@notreal.com", age: 45},
+    {name: "Peter Pan", email: "peter.pan@notreal.com", age: 100}
+];
+
+export default contacts;
